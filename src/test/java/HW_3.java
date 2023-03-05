c73d8509b4c0f129e3db11946a67f21adc55f1d3
Mikhail Lebedev <mhl.lbdvvv@gmail.com>
2023-03-06 01:52:46 +0300

    HW3

--- src/test/java/HW_3.java
+++ src/test/java/HW_3.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -15,12 +17,13 @@ public class HW_3 {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
     void fillFormTest() {
         open("https://demoqa.com/automation-practice-form");
-        Selenide.executeJavaScript("$('footer').remove()");
+        Selenide.executeJavaScript("$('footer').remove()"); // убираем баннер
 
 
         // напоминалка id=#, class=.
@@ -35,11 +38,12 @@ public class HW_3 {
         $(".react-datepicker__year-select").$(byText("2000")).click();
         $(".react-datepicker__month-select").$(byText("January")).click();
         $(".react-datepicker__month").$(byText("1")).click();
-//        $("#subjectsContainer").setValue("Subjects");
+        $("#subjectsInput").setValue("History").pressEnter();
         $("#hobbies-checkbox-1").parent().$(byText("Sports")).click();
         $("#hobbies-checkbox-2").parent().$(byText("Reading")).click();
         $("#hobbies-checkbox-3").parent().$(byText("Music")).click();
-//        $("#uploadPicture").uploadFromClasspath("");
+        $("#uploadPicture").uploadFile(new File("src/test/resources/pictures/HW3.png"));
+// альтернатиынй       $("#uploadPicture").uploadFromClasspath("pictures/HW3.png");
         $("#currentAddress").setValue("Current Address");
         $("#state").click();
         $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
@@ -48,6 +52,8 @@ public class HW_3 {
         $("#submit").sendKeys(Keys.ENTER);     // ?? click();
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("M L"), text("H_W_3@gmail.com"), text("Male"), text("8005553535"), text("01 January,2000"),
+                text("History"), text("Sports, Reading, Music"), text("HW3.png"), text("Current Address"), text(""), text("Uttar Pradesh Agra"));
 
     }
 }
